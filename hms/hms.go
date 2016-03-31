bc1e6a59d5fdcc0aa8d72bee5f7edcc128939f98
Jordon Wing <jordonwii@gmail.com>
2016-03-30 23:15:51 -0700

    Added temporary music updater handler.

--- hms/hms.go
+++ hms/hms.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
-	//"net/url"
+	"net/url"
 	"strconv"
 	"strings"
 	"time"
@@ -16,6 +17,7 @@ import (
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
 	"google.golang.org/appengine/user"
 )
 
@@ -33,6 +35,7 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	http.HandleFunc("/add_api_key", APIKeyAddHandler)
+	http.HandleFunc("/update_music_links", UpdateAllLinksWithMusicInfoHandler)
 	http.HandleFunc("/add_chat", ChatAddHandler)
 	http.HandleFunc("/backup", BackupLinksHandler)
 	http.Handle("/api/", appHandler(APIHandler))
@@ -173,6 +176,99 @@ func APIKeyAddHandler(w http.ResponseWriter, r *http.Request) {
 
 				}
 			}
+		}
+	}
+}
+
+func UpdateAllLinksWithMusicInfoHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		loginUrl, _ := user.LoginURL(c, r.URL.RequestURI())
+		http.Redirect(w, r, loginUrl, http.StatusFound)
+		return
+	} else {
+		if !u.Admin {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("You're not an admin. Go away."))
+		} else {
+			limit := 50
+			q := datastore.NewQuery("Link").Limit(limit)
+
+			converted := r.FormValue("converted")
+			strProgressed := r.FormValue("progressed")
+			cursorStr := r.FormValue("cursor")
+
+			if cursorStr != "" {
+				cursor, _ := datastore.DecodeCursor(cursorStr)
+				q = q.Start(cursor)
+			}
+
+			t := q.Run(c)
+			updated := 0
+			runCount := 0
+			for {
+				var l Link
+				key, err := t.Next(&l)
+				if err != nil {
+					if err == datastore.Done {
+						break
+					} else {
+						log.Errorf(c, "Error fetching next link: %v", err)
+						break
+					}
+				}
+
+				if l.IsLikelyMusicLink() && l.MusicInfo.Title == "" {
+					var info MusicInfo
+					client := urlfetch.Client(c)
+					params := url.Values{}
+					params.Set("link", l.TargetURL)
+
+					// TODO implement a task queue operation to fill in the info if this request fails.
+					resp, err := client.Get("http://music.hms.space/get_music_info?" + params.Encode())
+					if err != nil {
+						log.Errorf(c, "Request for music info for %v failed. Error: %v", l.TargetURL, err.Error())
+					} else {
+						defer resp.Body.Close()
+						body, err := ioutil.ReadAll(resp.Body)
+						if err != nil {
+							log.Errorf(c, "Failed to read body: %v", err.Error())
+						} else {
+							err = json.Unmarshal(body, &info)
+							if err != nil {
+								log.Errorf(c, "Failed to parse music response json: %v; json was %v", err.Error(), body)
+							} else {
+								l.MusicInfo = info
+								datastore.Put(c, key, &l)
+								updated += 1
+							}
+						}
+					}
+				}
+				runCount += 1
+			}
+
+			total_updated := updated
+			progressed := runCount
+
+			if converted != "" {
+				iConverted, _ := strconv.Atoi(converted)
+				total_updated += iConverted
+			}
+			if strProgressed != "" {
+				iProgressed, _ := strconv.Atoi(strProgressed)
+				progressed += iProgressed
+
+			}
+			if cursor, err := t.Cursor(); err == nil && runCount != 0 {
+
+				w.Header().Set("Content-Type", "text/html")
+				w.Write([]byte(fmt.Sprintf("<a href='http://hms.space/update_music_links?progressed=%v&converted=%v&cursor=%v'>%v progressed, %v more converted (%v total)!</a>", progressed, total_updated, cursor, progressed, updated, total_updated)))
+			} else {
+				w.Write([]byte(fmt.Sprintf("Finished! %v total converted, %v total analyzed.", total_updated, progressed)))
+			}
+
 		}
 	}
 }
